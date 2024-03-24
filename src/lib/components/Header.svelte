a1c97053b76b43272a46e5dbbe6f3999e93ed780
Aakash Goplani <goplaniaakash14@gmail.com>
2024-03-25 01:24:35 +0530

    clean(repo): removed sveltekit's link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,7 +6,7 @@
 
 <header>
 	<div class="corner">
-		<a href="https://kit.svelte.dev">
+		<a href="/">
 			<img src={logo} alt="SvelteKit" />
 		</a>
 	</div>
@@ -32,7 +32,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="/">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
